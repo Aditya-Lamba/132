afcd0872863a0947b1b187ce65979c79ad958fed
Aditya-Lamba <78349391+Aditya-Lamba@users.noreply.github.com>
2021-08-23 13:55:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+img = "";
+
+function TV()
+{
+    window.location = "tv.html";
+}
+function back()
+{
+    window.location = "index.html";
+}
+function Bottle()
+{
+    window.location = "bottle.html";
+}
+function Desk()
+{
+    window.location = "desk.html";
+}
